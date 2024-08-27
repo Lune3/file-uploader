49a0c58f030309f4e4de50f605668fff981c604e
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-08-27 23:38:01 +0530

    Working toward authentication added basic register

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require("node:path");
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const expressSession = require('express-session');
+
 
 const app = express();
 
+app.use(
+    expressSession({
+      cookie: {
+       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+      },
+      secret: 'a santa at nasa',
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(
+        prisma,
+        {
+          checkPeriod: 2 * 60 * 1000,  //ms
+          dbRecordIdIsSessionId: true,
+          dbRecordIdFunction: undefined,
+        }
+      )
+    })
+  );
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
