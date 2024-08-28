95005259900d1f3c029fc48a0852a700a1ffc16b
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-08-28 20:01:16 +0530

    changed default value of express session

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
-
 const express = require('express');
 const path = require("node:path");
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const expressSession = require('express-session');
+const passport = require('passport');
+const passportLocal = require('passport-local');
 
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true}));
 
 app.use(
     expressSession({
       cookie: {
-       maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+        maxAge: 7 * 24 * 60 * 60 * 1000 // ms
       },
       secret: 'a santa at nasa',
-      resave: true,
-      saveUninitialized: true,
+      resave: false,
+      saveUninitialized: false,
       store: new PrismaSessionStore(
         prisma,
         {
@@ -29,9 +34,8 @@ app.use(
     })
   );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
+app.use(passport.session());
+  
 app.get("/",(req,res) => {
     res.render("index");
 })
