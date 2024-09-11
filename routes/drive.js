a08f758b7d3bb4dee20a83fc2d3cefba8c5072fd
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-09-11 09:59:38 +0530

    File upload in root folder

--- routes/drive.js
+++ routes/drive.js
@@ -1,7 +1,14 @@
 const {Router} = require('express');
 const prisma = require('../db.js');
-
+const multer  = require('multer');
+const cloudinary = require('cloudinary').v2;
 const drive = Router();
+
+cloudinary.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret
+  });
 
 drive.get('/',async (req,res) => {
     const folders = await prisma.folder.findMany({
@@ -27,6 +34,27 @@ drive.post('/newFolder',async(req,res) => {
     res.redirect(`/drive`);
 });
 
+drive.get('/uploadFile', async (req,res) => {
+    res.render("file");
+});
+
+drive.post('/uploadFile' ,async(req,res) => {
+    const userFile = req.body.userFile;
+
+    const fileSize = req.file.size;
+    const fileSizeInKb = (fileSize / 1024).toFixed(2);
+    const fileSizeInMb = (fileSize / (1024 * 1024)).toFixed(2);
+
+    const newFile = await prisma.file.create({
+        data:{
+            name:userFile,
+            folderId:req.session.rootFolder,
+            size:fileSizeInKb > 1000?fileSizeInMb:fileSizeInKb
+        }
+    })
+    console.log(newFile);
+})
+
 drive.get('/:folderId',async (req,res) => {
     const selectedFolder = await prisma.folder.findMany({
         where:{
@@ -50,6 +78,7 @@ drive.post('/newFolder/:folderId',async (req,res) => {
     })
     res.redirect(`/drive/${parentFolder}`);
 });
+
 
 
 module.exports = drive;
